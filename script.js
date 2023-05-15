79af182f9704f136e21fe17904f91e7bb4db7741
deepak-dev1997 <108848941+deepak-dev1997@users.noreply.github.com>
2023-05-15 22:44:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
-//your JS code here. If required.
-let student={name: "deepak"}
-student.prototype.getKeys()=function getKeys(){
-	let arr=[];
-	for(let key in this){
-		arr.push(key);
-	}
-	return arr;
-}
+const student = {
+  name: "deepak"
+};
+
+Object.prototype.getKeys = function() {
+  return Object.keys(this);
+};
